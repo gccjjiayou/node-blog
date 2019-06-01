204107d842a9ee9d27d12b5be0cf6840bf47561a
gc <gc@email.com>
2019-06-01 17:20:42 +0800

    基本实现banner

--- route/admin.js
+++ route/admin.js
@@ -9,12 +9,12 @@ var db = mysql.createPool({
   database: 'learn'
 })
 
-module.exports = function() {
+module.exports = function () {
   var router = express.Router()
 
   // 检查登录状态
   router.use((req, res, next) => {
-    if(!req.session['admin_id'] && req.url != '/login') {  // 没有登录
+    if (!req.session['admin_id'] && req.url != '/login') {  // 没有登录
       res.redirect('/admin/login')
     } else {
       next()
@@ -27,17 +27,17 @@ module.exports = function() {
     var username = req.body.username
     var password = common.md5(req.body.password + common['MD5_SUFFIX'])
     db.query(`SELECT * FROM admin_table WHERE username='${username}'`, (err, data) => {
-      if(err) {
+      if (err) {
         console.log(err)
         res.status(500).send('database error').end()
       } else {
-        if(data.length === 0) {
+        if (data.length === 0) {
           res.status(400).send('no this admin').end()
         } else {
-          if(data[0].password === password) {
+          if (data[0].password === password) {
             req.session['admin_id'] = data[0].ID
             res.redirect('/admin')
-          }  else {
+          } else {
             res.status(400).send('this password is incorrect').end()
           }
         }
@@ -45,7 +45,79 @@ module.exports = function() {
     })
   })
   router.get('/', (req, res) => {
-    res.send('恭喜登录成功').end()
+    res.render('./admin/index.ejs', {})
+  })
+  router.get('/banners', (req, res) => {
+    switch (req.query.act) {
+      case 'mod':
+        db.query(`SELECT * FROM banner_table WHERE ID=${req.query.id}`, (err, data) => {
+          if (err) {
+            console.log(err)
+            res.status(500).send('database error').end()
+          }
+          else if (data[0].length === 0) {
+            res.status(404).send('data not found').end()
+          }
+          else {
+            db.query(`SELECT * FROM banner_table`, (err, banners) => {
+              if (err) {
+                console.log(err)
+                res.status(500).send('database error').end()
+              } else {
+                console.log('data[0]: ', data[0])
+                res.render('./admin/banners.ejs', { banners, mod_data: data[0] })
+              }
+            })
+          }
+        })
+        break
+      case 'del':
+        db.query(`DELETE FROM banner_table WHERE ID=${req.query.id}`, (err, data) => {
+          if (err) {
+            console.log(err)
+            res.status(500).send('database error').end()
+          } else {
+            res.redirect('/admin/banners')
+          }
+        })
+        break;
+      default:
+        db.query(`SELECT * FROM banner_table`, (err, banners) => {
+          if (err) {
+            console.log(err)
+            res.status(500).send('database error').end()
+          }
+          res.render('./admin/banners.ejs', { banners })
+        })
+
+    }
+
+  })
+  router.post('/banners', (req, res) => {
+    var title = req.body.title
+    var description = req.body.description
+    var href = req.body.href
+    if (!title || !description || !href) {
+      res.status(400).send('arg error').end()
+    } else {
+      if (req.body.mod_id) { // 修改
+        db.query(`UPDATE banner_table SET title='${req.body.title}', description='${req.body.description}', href='${req.body.href}' WHERE ID=${req.body.mod_id}`, (err, data) => {
+          if (err) {
+            console.log(err)
+            res.status(500).send('database error').end()
+          } else {
+            db.query(`INSERT INTO banner_table(title, description, href) VALUE('${title}', '${description}', '${href}')`, (err, data) => {
+              if (err) {
+                console.log(err)
+                res.status(500).send('database error').end()
+              } else {
+                res.redirect('/admin/banners')
+              }
+            })
+          }
+        })
+      }
+    }
   })
   return router
 }
